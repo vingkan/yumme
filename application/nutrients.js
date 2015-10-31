1cba79e25423d15f308a8bb80b14b772d4858a64
Recursions <Nick@TellMyWiFiILoveHer.local>
2015-10-31 10:51:25 -0500

    Inputs and button

--- application/nutrients.js
+++ application/nutrients.js
@@ -1,2 +1,4 @@
+var input = document.getElementById("textBox");
+var submit = docuemnt.getElementById("submitButton");
 var apiKey = "WuhJZNLfyq9hTyTQpPAjUC3wYG5wA2hIfS01XOVl";
-var link = "http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=";DEMO_KEY&nutrients=205&nutrients=204&nutrients=208&nutrients=269
+var link = "http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=";//DEMO_KEY&nutrients=205&nutrients=204&nutrients=208&nutrients=269
