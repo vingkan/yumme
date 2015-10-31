573430237408db1dd32c4cf5437cff047faf3291
Vinesh Kannan <vingkan@gmail.com>
2015-10-31 15:33:30 -0500

    Set sidebartoggle()

--- view/view.js
+++ view/view.js
@@ -1,9 +1,9 @@
-document.getElementById('bar').style.left = "0vw";
+document.getElementById('cart').style.left = "-52vw";
 
 function toggleSideBar(sideBarID){
 	var sideBar = document.getElementById(sideBarID);
 	if(sideBar.style.left == '0vw'){
-		sideBar.style.left = '-50vw';
+		sideBar.style.left = '-52vw';
 	}
 	else{
 		sideBar.style.left = '0vw';
