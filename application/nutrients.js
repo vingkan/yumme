0b57d5f2487238663cd9b9e83a7fe6af7ac6a7bd
Recursions <Nick@TellMyWiFiILoveHer.local>
2015-10-31 11:03:05 -0500

    my name is nick li and i suck at commits

--- application/nutrients.js
+++ application/nutrients.js
@@ -2,3 +2,6 @@ var input = document.getElementById("textBox");
 var submit = docuemnt.getElementById("submitButton");
 var apiKey = "WuhJZNLfyq9hTyTQpPAjUC3wYG5wA2hIfS01XOVl";
 var link = "http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=";//DEMO_KEY&nutrients=205&nutrients=204&nutrients=208&nutrients=269
+
+function findNutrient() {
+}
