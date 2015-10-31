014b0caeef60bac7e65cee5f4603a916e69784ae
Recursions <Nick@TellMyWiFiILoveHer.local>
2015-10-31 13:08:32 -0500

    Mapping Nutrient JSON to objects array

--- application/nutrients.js
+++ application/nutrients.js
@@ -1,19 +1,28 @@
+var input = document.getElementById("textBox");
+var output = document.getElementById("output");
 // To get information from the food database you have to do two searches
 // The first search takes an string
 var apiKey = "WuhJZNLfyq9hTyTQpPAjUC3wYG5wA2hIfS01XOVl";
+
+var database;
+
 // First search variables
-var first = "http://api.nal.usda.gov/ndb/search/?format=json&q="/* Search Input */
-                                                      +"&sort=n"// "n" sort by food name, "r" sort by relevance 
-                                                      +"&max=25"// maximum rows to return
-                                                    +"&offset=0"// offset of the beginning row
-                                                      +"&api_key="+apiKey;
+//function search(name, sort, max, offset) {
+//    this.name = name; // search terms
+//    this.sort = sort; // "n" sort by food name, "r" sort by relevance 
+//    this.max = max; // maximum rows to return
+//    this.offset = offset; // offset of the beginning row
+//    this.getURL = function() {
+//        return "http://api.nal.usda.gov/ndb/search/?format=json&q="+this.name+"&sort="+this.sort+"&max="+this.max+"&offset="+this.offset+"&api_key="+apiKey;
+//    }
+//}
 
-
-var input = document.getElementById("textBox");
-var output = document.getElementById("output");
-var apiKey = "WuhJZNLfyq9hTyTQpPAjUC3wYG5wA2hIfS01XOVl";
-var link = "http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=";//DEMO_KEY&nutrients=205&nutrients=204&nutrients=208&nutrients=269
 
 function findNutrient() {
-    output.innerHTML += first
+    $.getJSON("http://api.nal.usda.gov/ndb/search/?format=json&q=butter&sort=n&max=25&offset=0&api_key="+apiKey, function(data) {
+        //var obj = jQuery.parseJSON(data);
+        database = data['list']['item'];
+        
+        //output.innerHTML += 
+    });
 }
