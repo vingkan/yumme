18a24076b488e431d9a485fc37398df526474d8e
Recursions <Nick@TellMyWiFiILoveHer.local>
2015-10-31 10:39:39 -0500

    Added nutrients.js
    Has the api key from usda.gov for access to nutrients of food

---
+++ application/nutrients.js
@@ -0,0 +1,2 @@
+var apiKey = "WuhJZNLfyq9hTyTQpPAjUC3wYG5wA2hIfS01XOVl";
+var link = "http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=";DEMO_KEY&nutrients=205&nutrients=204&nutrients=208&nutrients=269
