192d5222686cefeca82d76027e12837a09a64ee0
Vinesh Kannan <vingkan@gmail.com>
2015-10-31 13:07:29 -0500

    Styled table location

--- data/prepSpace.js
+++ data/prepSpace.js
@@ -12,14 +12,14 @@ PrepSpace.prototype.toHTML = function(){
 	html += 'id="' + this.name + '" ';
 	html += 'style="background-image: url(' + this.image + ');"';
 	html += '>'
-	html += this.name;
+	html += '<label>' + this.name + '</label>';
 	html += '</div>'
 	return html;
 }
 
 var table = new PrepSpace({
 	name: 'Table',
-	image: 'style/img/strawberry.png'
+	image: 'style/img/table.jpg'
 });
 console.log(table.toHTML());
 document.getElementById('locationSpace').innerHTML = table.toHTML();
