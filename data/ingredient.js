edae483a3fc7c4190dda86e25c4215b136b0713e
Vinesh Kannan <vingkan@gmail.com>
2015-10-31 13:43:57 -0500

    Added TouchPunch Drag and Drop

--- data/ingredient.js
+++ data/ingredient.js
@@ -1,7 +1,20 @@
 Ingredient.prototype.name = ""; //String
+Ingredient.prototype.id = ""; //String
 Ingredient.prototype.image = ""; //Path
 
 function Ingredient(config){
 	this.name = config['name'];
+	this.id = config['id'];
 	this.image = config['image'];
+}
+
+Ingredient.prototype.toHTML = function(){
+	var html = '';
+	html += '<div class="ingredient" ';
+	html += 'id="' + this.id + '" ';
+	html += 'style="background-image: url(' + this.image + ');"';
+	html += '>'
+	html += '<label>' + this.name + '</label>';
+	html += '</div>'
+	return html;
 }
--- data/prepSpace.js
+++ data/prepSpace.js
@@ -1,25 +1,20 @@
 PrepSpace.prototype.name = ""; //String
+PrepSpace.prototype.id = ""; //String
 PrepSpace.prototype.image = ""; //Path
 
 function PrepSpace(config){
 	this.name = config['name'];
+	this.id = config['id'];
 	this.image = "'" + config['image'] + "'";
 }
 
 PrepSpace.prototype.toHTML = function(){
 	var html = '';
 	html += '<div class="location" ';
-	html += 'id="' + this.name + '" ';
+	html += 'id="' + this.id + '" ';
 	html += 'style="background-image: url(' + this.image + ');"';
 	html += '>'
 	html += '<label>' + this.name + '</label>';
 	html += '</div>'
 	return html;
 }
-
-var table = new PrepSpace({
-	name: 'Table',
-	image: 'style/img/table.jpg'
-});
-console.log(table.toHTML());
-document.getElementById('locationSpace').innerHTML = table.toHTML();
