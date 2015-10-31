0dc60aca745b88e9ffe9442ff6b7a643a93295b4
Recursions <Nick@TellMyWiFiILoveHer.local>
2015-10-31 11:19:19 -0500

    working on finding outputting results

--- application/nutrients.js
+++ application/nutrients.js
@@ -1,7 +1,8 @@
 var input = document.getElementById("textBox");
-var submit = docuemnt.getElementById("submitButton");
+var output = document.getElementById("output");
 var apiKey = "WuhJZNLfyq9hTyTQpPAjUC3wYG5wA2hIfS01XOVl";
 var link = "http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=";//DEMO_KEY&nutrients=205&nutrients=204&nutrients=208&nutrients=269
 
 function findNutrient() {
+    console.log(input.value);
 }
