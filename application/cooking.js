1b0d0d17214eea68eda8f9f1ba179285faacd810
Vinesh Kannan <vingkan@gmail.com>
2015-11-01 00:36:30 -0500

    Added Utensil.toHTML()

---
+++ application/cooking.js
@@ -0,0 +1,52 @@
+var plate = new PrepSpace({
+	name: 'My Plate',
+	id: 'plate',
+	image: 'style/img/plate.png'
+});
+document.getElementById('locationSpace').innerHTML += plate.toHTML();
+
+var sink = new PrepSpace({
+	name: 'Sink',
+	id: 'sink',
+	image: 'style/img/sink.png'
+});
+document.getElementById('locationSpace').innerHTML += sink.toHTML();
+
+var table = new PrepSpace({
+	name: 'Table',
+	id: 'table',
+	image: 'style/img/table.jpg'
+});
+document.getElementById('locationSpace').innerHTML += table.toHTML();
+
+var strawberry = new Ingredient({
+	name: 'Strawberry',
+	id: 'strawberry',
+	image: 'style/img/strawberry.png'
+});
+document.getElementById('container-cart').innerHTML += strawberry.toHTML();
+
+var honey = new Ingredient({
+	name: 'Honey',
+	id: 'honey',
+	image: 'style/img/honey.png'
+});
+document.getElementById('container-cart').innerHTML += honey.toHTML();
+
+var faucet = new Utensil({
+	name: 'Faucet',
+	id: 'faucet',
+	width: 107.686,
+	height: 213.472,
+	image: 'style/img/faucet-off.png'
+});
+document.getElementById('sink').innerHTML += faucet.toHTML();
+
+$('#' + strawberry.id).draggable();
+$('#' + honey.id).draggable();
+
+$('#' + faucet.id).draggable();
+
+acceptDrops('plate')
+acceptDrops('sink')
+acceptDrops('table')
--- data/utensil.js
+++ data/utensil.js
@@ -4,6 +4,34 @@ Utensil.prototype.image = ""; //Path
 
 function Utensil(config){
 	this.name = config['name'];
+	this.width = parseFloat(config['width']);
+	this.height = parseFloat(config['height']);
 	this.id = config['id'];
 	this.image = config['image'];
+}
+
+Utensil.prototype.getScaledWidth = function(){
+	var scaled = (90.0 / 400.0) * (this.width);
+	console.log('getScaledWidth = ' + scaled);
+	return scaled;
+}
+
+Utensil.prototype.getScaledHeight = function(){
+	var scaled = (90.0 / 400.0) * (this.height);
+	console.log('getScaledHeight = ' + scaled);
+	return scaled;
+}
+
+Utensil.prototype.toHTML = function(){
+	var html = '';
+	html += '<div class="utensil" ';
+	html += 'id="' + this.id + '" ';
+	html += 'style="width: ' + this.getScaledWidth() + 'vw;';
+	html += 'height: ' + this.getScaledHeight() + 'vw;';
+	html += 'background-image: url(' + this.image + ');"';
+	html += '>';
+	html += '<span class="label">' + this.name + '</span>';
+	html += '</div>';
+	console.log(html)
+	return html;
 }
