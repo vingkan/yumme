b6feabf5c8bc4cbad2d2a8642360bdfe9800a214
Vinesh Kannan <vingkan@gmail.com>
2015-11-01 03:34:41 -0600

    Added Recipe.toHTML()

--- data/recipe.js
+++ data/recipe.js
@@ -1,9 +1,23 @@
 Recipe.prototype.name = ""; //String
 Recipe.prototype.id = ""; //String
 Recipe.prototype.ingredients = []; //Array of Ingredient Objects
+Recipe.prototype.instructions = []; //Array of Strings
 
 function Recipe(config){
 	this.name = config['name'];
 	this.id = config['id'];
 	this.ingredients = config['ingredients']
+	this.instructions = config['instructions'];
+}
+
+Recipe.prototype.toHTML = function(){
+	var html = "";
+	html += '<h3>' + this.name + '</h3>';
+	html += '<ol>';
+	var size = instructions.length;
+	for(var i = 0; i < size; i++){
+		html += '<li>' + instructions[i] + '</li>';
+	}
+	html += '</ol>';
+	return html;
 }
